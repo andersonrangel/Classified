e37009c2749f1be986c5f21238a134c337927cf9
Anderson Rangel <Anderson.Rangel@red-gate.com>
2020-09-01 00:02:58 +1000

    initial commit

---
+++ Classfied/Programmable Objects/dbo/Stored Procedures/GetContacts.sql
@@ -0,0 +1,33 @@
+IF OBJECT_ID('[dbo].[GetContacts]') IS NOT NULL
+	DROP PROCEDURE [dbo].[GetContacts];
+
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+--SET QUOTED_IDENTIFIER ON|OFF
+--SET ANSI_NULLS ON|OFF
+CREATE PROCEDURE [dbo].[GetContacts]
+--Author:sa
+--Date: 26/11/2019
+--GO
+    @parameter_name AS INT
+-- WITH ENCRYPTION, RECOMPILE, EXECUTE AS CALLER|SELF|OWNER| 'user_name'
+AS
+--SET NOCOUNT ON
+
+    SELECT	DC.customer_id,
+            DC.customer_firstname,
+            DC.customer_lastname,
+            DC.customer_gender,
+            DC.customer_company_name,
+            DC.customer_street_address,
+            DC.customer_region,
+            DC.customer_country,
+            DC.customer_email,
+            DC.customer_telephone,
+            DC.customer_zipcode,
+            DC.credit_card_type_id,
+            DC.customer_credit_card_number FROM dbo.DM_CUSTOMER AS DC
+GO
