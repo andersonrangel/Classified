b888a7d8f717ab475908d5b8e2b49aa01f885bfe
Anderson Rangel <Anderson.Rangel@red-gate.com>
2020-09-02 09:26:00 +1000

    delete old proc

---
+++ Classfied/Migrations/1.1.0-Changes/005_20200902-0925_Anderson.Rangel.sql
@@ -0,0 +1,8 @@
+﻿-- <Migration ID="9408f93b-d09c-4b76-8930-b2f401d23411" />
+GO
+
+PRINT N'Dropping [dbo].[oldproc]'
+GO
+IF OBJECT_ID(N'[dbo].[oldproc]', 'P') IS NOT NULL
+DROP PROCEDURE [dbo].[oldproc]
+GO
