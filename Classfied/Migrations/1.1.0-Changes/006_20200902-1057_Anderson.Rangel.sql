58aafbee0aa0bf133cca18c15da3959ce85a613e
Anderson Rangel <Anderson.Rangel@red-gate.com>
2020-09-02 10:58:42 +1000

    comments should be great

---
+++ Classfied/Migrations/1.1.0-Changes/006_20200902-1057_Anderson.Rangel.sql
@@ -0,0 +1,13 @@
+﻿-- <Migration ID="b1aa78fc-f6e2-49f5-98c8-28afe54bd871" />
+GO
+
+PRINT N'Dropping [dbo].[oldproc]'
+GO
+IF OBJECT_ID(N'[dbo].[oldproc]', 'P') IS NOT NULL
+DROP PROCEDURE [dbo].[oldproc]
+GO
+PRINT N'Altering [dbo].[DM_CUSTOMER]'
+GO
+IF COL_LENGTH(N'[dbo].[DM_CUSTOMER]', N'status') IS NULL
+ALTER TABLE [dbo].[DM_CUSTOMER] ADD[status] [int] NULL
+GO
---
+++ Classfied/Migrations/1.1.0-Changes/007_20200902-1057_Anderson.Rangel.sql
@@ -0,0 +1,3 @@
+﻿-- <Migration ID="4bfbcff1-1a3e-45e9-8c19-c79af93a1e7f" />
+GO
+UPDATE dbo.DM_CUSTOMER SET	status  =1
